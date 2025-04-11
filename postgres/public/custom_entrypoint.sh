70587a5174a1221894867355741a6072e8aac9d5
Deo <liangzhanzhao@metrodata.cn>
2025-04-11 15:08:52 +0800

    sync with internal v*.21.2

--- postgres/public/custom_entrypoint.sh
+++ postgres/public/custom_entrypoint.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 set -Eeo pipefail
 
+# 检查 MDT_CRON 环境变量是否存在
+if [ "$(id -u)" -eq 0 ] && [ -n "$MDT_CRON" ]; then
+  # 将环境变量写入到 crontab 文件
+  echo "$MDT_CRON" > /etc/cron.d/crontab
+  # 确保权限设置正确
+  chmod 0644 /etc/cron.d/crontab
+  crontab /etc/cron.d/crontab
+
+  # 启动 cron 服务
+  service cron start
+fi
+
 source docker-entrypoint.sh
 if [ "$#" -eq 0 ] || [ "$1" != 'postgres' ]; then
   set -- postgres "$@"
